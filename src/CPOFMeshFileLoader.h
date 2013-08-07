47ad87d425a5eb1394cafeee574a0d78823386b5
caasi Huang <caasi.igd@gmail.com>
2013-08-08 07:30:03 +0800

    change indent for GitHub

--- src/CPOFMeshFileLoader.h
+++ src/CPOFMeshFileLoader.h
@@ -5,24 +5,24 @@
 
 #include "IMeshLoader.h"
 
-typedef	int32_t			POF_INT;
-typedef	uint32_t		POF_UINT;
-typedef	int16_t			POF_SHORT;
-typedef	uint16_t		POF_USHORT;
+typedef	int32_t				POF_INT;
+typedef	uint32_t			POF_UINT;
+typedef	int16_t				POF_SHORT;
+typedef	uint16_t			POF_USHORT;
 typedef	signed char		POF_CHAR;
 typedef	unsigned char	POF_UBYTE;
-typedef	float			POF_FLOAT;
+typedef	float					POF_FLOAT;
 typedef	struct pof_vector
 {
 	POF_FLOAT x;
 	POF_FLOAT y;
 	POF_FLOAT z;
-}						POF_VECTOR;
+}											POF_VECTOR;
 typedef struct pof_string
 {
 	POF_INT length;
 	POF_CHAR* text;	// it's not null-terminated in the POF files
-}						POF_STRING;
+}											POF_STRING;
 
 namespace irr
 {
