00856f4547d30004df70738103c694dd81191a8e
caasi Huang <caasi.igd@gmail.com>
2013-08-08 09:15:48 +0800

    first window

--- src/main.cpp
+++ src/main.cpp
@@ -19,5 +19,27 @@ int main(int argc, char* argv[])
 {
 	scene::CPOFMeshFileLoader pof_loader(NULL, NULL);
 
+	IrrlichtDevice *device;
+
+	device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(640, 480), 32);
+	if (!device) return 1;
+	device->setWindowCaption(L"POF Viewer");
+
+	video::IVideoDriver* driver = device->getVideoDriver();
+	scene::ISceneManager* smgr = device->getSceneManager();
+	gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+	while(device->run())
+	{
+		driver->beginScene(true, true, video::SColor(255, 22, 22, 29));
+
+		smgr->drawAll();
+		guienv->drawAll();
+
+		driver->endScene();
+	}
+
+	device->drop();
+
 	return 0;
 }
