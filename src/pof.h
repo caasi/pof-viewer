a0e0f4e4de2a89adbc5a9233d35ec159dc29c515
caasi Huang <caasi.igd@gmail.com>
2013-08-16 09:28:48 +0800

    readability

--- src/pof.h
+++ src/pof.h
@@ -12,23 +12,23 @@
 /* structs */
 typedef struct pof_header
 {
-	POF_INT file_id;	// for convenience
-	POF_INT version;
+	POF_INT		file_id;	// for convenience
+	POF_INT		version;
 } POFHeader;
 typedef struct pof_chunk_header
 {
-	POF_INT chunk_id;
-	POF_INT length;
+	POF_INT		chunk_id;
+	POF_INT		length;
 } POFChunkHeader;
 typedef struct pof_chunk_hdr2_cross_section
 {
-	POF_FLOAT depth;
-	POF_FLOAT radius;
+	POF_FLOAT	depth;
+	POF_FLOAT	radius;
 } POFChunkCrossSection;
 typedef struct pof_chunk_hdr2_light
 {
-	POF_VECTOR location;
-	POF_INT light_type;
+	POF_VECTOR	location;
+	POF_INT		light_type;
 } POFChunkLight;
 typedef struct pof_chunk_hdr2
 {
@@ -42,92 +42,92 @@ typedef struct pof_chunk_obj2
 /* IDTA */
 typedef struct pof_bsp_eof
 {
-	POF_INT id;
-	POF_INT size;
+	POF_INT		id;
+	POF_INT		size;
 } BSPEndOfFile;
 typedef struct pof_bsp_vertex
 {
-	POF_VECTOR point;
-	POF_VECTOR *normals;
+	POF_VECTOR	point;
+	POF_VECTOR	*normals;
 } BSPVertex;
 typedef struct pof_bsp_flatpoly
 {
-	POF_INT id;
-	POF_INT size;
-	POF_INT n_verts;
-	POF_INT n_norms;
-	POF_INT offset;
-	POF_CHAR *norm_counts;
-	BSPVertex *vertex_data;
+	POF_INT		id;
+	POF_INT		size;
+	POF_INT		n_verts;
+	POF_INT		n_norms;
+	POF_INT		offset;
+	POF_CHAR	*norm_counts;
+	BSPVertex	*vertex_data;
 } BSPFlatPolygon;
 typedef struct pof_bsp_tmapvertex
 {
-	POF_SHORT vert_num;
-	POF_SHORT norm_num;
-	POF_FLOAT u;
-	POF_FLOAT v;
+	POF_SHORT	vert_num;
+	POF_SHORT	norm_num;
+	POF_FLOAT	u;
+	POF_FLOAT	v;
 } BSPTexturedVertex;
 typedef struct  pof_bsp_tmappoly
 {
-	POF_INT id;
-	POF_INT size;
-	POF_VECTOR normal;
-	POF_VECTOR center;
-	POF_FLOAT radius;
-	POF_INT n_verts;
-	POF_INT tmap_num;
-	BSPTexturedVertex *vertex_data;
+	POF_INT			id;
+	POF_INT			size;
+	POF_VECTOR		normal;
+	POF_VECTOR		center;
+	POF_FLOAT		radius;
+	POF_INT			n_verts;
+	POF_INT			tmap_num;
+	BSPTexturedVertex	*vertex_data;
 } BSPTexturedPolygon;
 typedef struct pof_bsp_sortnorm
 {
-	POF_INT id;
-	POF_INT size;
-	POF_VECTOR plane_normal;
-	POF_VECTOR plane_point;
-	POF_INT reserved;
-	POF_INT offset_front;
-	POF_INT offset_back;
-	POF_INT offset_prelist;
-	POF_INT offset_postlist;
-	POF_INT offset_online;
-	POF_VECTOR bounding_min;
-	POF_VECTOR bounding_max;
+	POF_INT		id;
+	POF_INT		size;
+	POF_VECTOR	plane_normal;
+	POF_VECTOR	plane_point;
+	POF_INT		reserved;
+	POF_INT		offset_front;
+	POF_INT		offset_back;
+	POF_INT		offset_prelist;
+	POF_INT		offset_postlist;
+	POF_INT		offset_online;
+	POF_VECTOR	bounding_min;
+	POF_VECTOR	bounding_max;
 } BSPSortNormal;
 typedef struct pof_bsp_boundbox
 {
-	POF_INT id;
-	POF_INT size;
-	POF_VECTOR min;
-	POF_VECTOR max;
+	POF_INT		id;
+	POF_INT		size;
+	POF_VECTOR	min;
+	POF_VECTOR	max;
 } BSPBoundBox;
 
 /* debug functions in C style for those structs */
 #define INDENT(num) {for (unsigned int i = 0; i < num; ++i) putchar('\t');}
 
-int	pof_string_build(POF_STRING *str, irr::io::IReadFile *file);
-void	pof_string_print(POF_STRING *str, unsigned int indent);
-void	pof_string_clean(POF_STRING *str);
+int	pof_string_build		(POF_STRING *str, irr::io::IReadFile *file);
+void	pof_string_print		(POF_STRING *str, unsigned int indent);
+void	pof_string_clean		(POF_STRING *str);
 
-void	pof_vector_print(POF_VECTOR *v, unsigned int indent);
+void	pof_vector_print		(POF_VECTOR *v, unsigned int indent);
 
-void	pof_header_print(POFHeader* header, unsigned int indent);
+void	pof_header_print		(POFHeader* header, unsigned int indent);
 
-void	pof_chunk_header_print(POFChunkHeader* header, unsigned int indent);
+void	pof_chunk_header_print		(POFChunkHeader* header, unsigned int indent);
 
-int	pof_chunk_hdr2_build(POFObject *obj, irr::io::IReadFile *file);
-void	pof_chunk_hdr2_print(POFObject *obj, unsigned int indent);
-void	pof_chunk_hdr2_clean(POFObject *obj);
+int	pof_chunk_hdr2_build		(POFObject *obj, irr::io::IReadFile *file);
+void	pof_chunk_hdr2_print		(POFObject *obj, unsigned int indent);
+void	pof_chunk_hdr2_clean		(POFObject *obj);
 
-int	pof_chunk_obj2_build(POFSubObject *obj, irr::io::IReadFile *file);
-void	pof_chunk_obj2_print(POFSubObject *obj, unsigned int indent);
-void	pof_chunk_obj2_clean(POFSubObject *obj);
+int	pof_chunk_obj2_build		(POFSubObject *obj, irr::io::IReadFile *file);
+void	pof_chunk_obj2_print		(POFSubObject *obj, unsigned int indent);
+void	pof_chunk_obj2_clean		(POFSubObject *obj);
 
-void	pof_bsp_eof_print(BSPEndOfFile *obj, unsigned int indent);
-void	pof_bsp_vertex_print(BSPVertex *obj, unsigned int indent);
-void	pof_bsp_flatpoly_print(BSPFlatPolygon *obj, unsigned int indent);
-void	pof_bsp_tmapvertex_print(BSPTexturedVertex *obj, unsigned int indent);
-void	pof_bsp_tmappoly_print(BSPTexturedPolygon *obj, unsigned int indent);
-void	pof_bsp_sortnorm_print(BSPSortNormal *obj, unsigned int indent);
-void	pof_bsp_boundbox_print(BSPBoundBox *obj, unsigned int indent);
+void	pof_bsp_eof_print		(BSPEndOfFile *obj, unsigned int indent);
+void	pof_bsp_vertex_print		(BSPVertex *obj, unsigned int indent);
+void	pof_bsp_flatpoly_print		(BSPFlatPolygon *obj, unsigned int indent);
+void	pof_bsp_tmapvertex_print	(BSPTexturedVertex *obj, unsigned int indent);
+void	pof_bsp_tmappoly_print		(BSPTexturedPolygon *obj, unsigned int indent);
+void	pof_bsp_sortnorm_print		(BSPSortNormal *obj, unsigned int indent);
+void	pof_bsp_boundbox_print		(BSPBoundBox *obj, unsigned int indent);
 
 #endif
